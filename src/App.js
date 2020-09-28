894b3affb5b2228cf985f3f41c4eaf76e5139436
slee713 <steven.lee1393@gmail.com>
2020-09-28 10:53:33 -0400

    added routes for home/account/reservations

--- src/App.js
+++ src/App.js
@@ -5,35 +5,43 @@ import RestContainer from './containers/RestContainer'
 import Navbar from './containers/Navbar'
 import Account from './components/Account'
 import Reservation from './components/Reservation'
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 let baseUrl= "http://localhost:3000/api/v1/"
 let restUrl = baseUrl + "restaurants"
 let loginUrl = baseUrl + 'login'
 
 class App extends React.Component{
-  state = {
-    page: "restaurants"
-  }
+  // state = {
+  //   page: "restaurants"
+  // }
 
-  changePage = page => {
-    this.setState({page})
-  }
+  // changePage = page => {
+  //   this.setState({page})
+  // }
 
 
   render(){
     return (
-      <div className="App">
-        <Navbar baseUrl={baseUrl} restUrl={restUrl} loginUrl={loginUrl} changePage={this.changePage}/>
-        {this.state.page === "restaurants" 
-        ? 
-        <RestContainer baseUrl={baseUrl} restUrl={restUrl} loginUrl={loginUrl}/>
-        :
-        this.state.page === "account" 
-        ?
-        <Account />:
-        <Reservation />}
-        
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar baseUrl={baseUrl} restUrl={restUrl} loginUrl={loginUrl} />
+          {/* {this.state.page === "restaurants" 
+          ? 
+          <RestContainer baseUrl={baseUrl} restUrl={restUrl} loginUrl={loginUrl}/>
+          :
+          this.state.page === "account" 
+          ?
+          <Account />: */}
+          {/* <Reservation />} */} 
+          <Switch>
+            <Route exact path="/" render={(routerProps)=>  <RestContainer {...routerProps} baseUrl={baseUrl} restUrl={restUrl} loginUrl={loginUrl}/>}/>
+            <Route exact path="/account" render={(routerProps)=>  <Account {...routerProps} baseUrl={baseUrl} restUrl={restUrl} loginUrl={loginUrl}/>}/>
+            <Route exact path="/reservations" render={(routerProps)=>  <Reservation {...routerProps} baseUrl={baseUrl} restUrl={restUrl} loginUrl={loginUrl}/>}/>
+          </Switch>
+
+
+        </div>
+        </BrowserRouter>
     );
   }
 }
--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -4,7 +4,7 @@ import Login from '../components/Login'
 import Signup from '../components/SignUp'
 import './Navbar.css'
 import '../yummy.png'
-
+import { Link } from 'react-router-dom';
 class Navbar extends React.Component {
     state={
         logged_in: localStorage.token ? true : false
@@ -26,12 +26,12 @@ class Navbar extends React.Component {
     render(){
         return(
             <div className="navbar">
-                <img className='logo' src={require('../yummy.png')} onClick={() => this.props.changePage("restaurants")}/>
+                <Link to="/"><img className='logo' src={require('../yummy.png')}/></Link>
                 {this.state.logged_in ?
                 <div className="loggedIn">
-                    <p onClick={() => this.props.changePage("account")}>My Account</p>
-                    <p onClick={()=> this.props.changePage("reservations")}>Reservations</p>
-                    <p onClick={() => this.logout()}>Logout</p>
+                    <Link to="/account" >My Account</Link>
+                    <Link to="/reservations" >Reservations</Link>
+                    <p onClick={this.logout} >Logout</p>
                 </div>
                 :
                 <div className="loggedOut">
