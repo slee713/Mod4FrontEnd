71dcfc4c4bec7c6b91b4d18c854ee4de3f12d939
slee713 <steven.lee1393@gmail.com>
2020-09-29 15:24:20 -0400

    show map of current rest and click on marker to open desc modal

--- src/containers/Map.js
+++ src/containers/Map.js
@@ -5,7 +5,7 @@ import {
     Marker,
     InfoWindow
 } from '@react-google-maps/api'
-
+import RestDesc from '../components/RestDesc'
 const libraries = ["places"]
 const mapContainerStyle = {
     width: '80vw',
@@ -16,11 +16,19 @@ const center = {
     lng: -77.036873
 }
 
+
+
 const Map = props => {
     const {isLoaded, loadError} = useLoadScript({
         googleMapsApiKey: process.env.REACT_APP_GOOGLES_MAPS_API_KEY,
         libraries
     })
+    const [selected, setSelected] = React.useState(null)
+
+    const mapRef = React.useRef();
+    const onMapLoad = React.useCallback(map=> {
+        mapRef.current = map
+    }, [])
 
     if(loadError) return "Error Loading Map"
     if(!isLoaded) return "Loading Maps"
@@ -32,6 +40,7 @@ const Map = props => {
                 mapContainerStyle={mapContainerStyle} 
                 zoom={10}
                 center={center}
+                onLoad={onMapLoad}
             >
                 {props.restaurants.map(rest => 
                     <Marker 
@@ -40,7 +49,17 @@ const Map = props => {
                             lat: parseFloat(rest.location.latitude),
                             lng: parseFloat(rest.location.longitude)
                         }}
+                        onClick={()=> setSelected(rest)}
                     />)}
+                {selected ? 
+                <InfoWindow 
+                    position={{lat: parseFloat(selected.location.latitude), lng: parseFloat(selected.location.longitude)}}
+                    onCloseClick={()=> setSelected(null)}>
+                    <div>
+                        <h2>{selected.name}</h2>
+                        <RestDesc restaurant={selected} />
+                    </div>
+                </InfoWindow> : null}
             </GoogleMap>
 
         </div>
