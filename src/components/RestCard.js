19e705d387e839b5a601e504f8eed14f6bef3b7d
ClaFiore <claborghini@gmail.com>
2020-09-30 17:29:32 -0400

    thumb images, and default images

--- src/components/RestCard.js
+++ src/components/RestCard.js
@@ -8,10 +8,17 @@ import './RestCard.css'
 import { Card, Image } from 'semantic-ui-react'
 
 const RestCard = (props) => {
+  
     const {name, location, rating, thumb, price_range} = props.restaurant
+    
+    
+    const rest_cuisine = props.restaurant.cuisines.split(',')[0].toLowerCase()
+    const available_cuisines = ['american', 'afghan', 'chinese', 'french', 'greek', 'indian', 'italian', 'korean', 'mexican', 'middle eastern', 'pakistani', 'seafood', 'thai', 'turkish', 'vegetarian']
+    
+
     return(
     <Card className ="card">
-    <Image className="img" src={thumb ? thumb : props.image} wrapped ui={false} />
+    <Image className="img" src={thumb ? thumb : available_cuisines.includes(rest_cuisine) ? require(`../cuisine_images/${rest_cuisine}.jpg`) : props.image} wrapped ui={false} />
     <Card.Content>
       <Card.Header>{name}</Card.Header>
       <Card.Description>
@@ -29,8 +36,9 @@ const RestCard = (props) => {
     )
 }
 // hash[cuisine.split(", ")[0]]
+
 RestCard.defaultProps = {
-    image: "https://library.kissclipart.com/20181211/lke/kissclipart-home-in-a-circle-icon-clipart-computer-icons-house-e3d4f94631e5fbef.jpg"
+  image: 'https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80' 
   };
 
 export default RestCard
